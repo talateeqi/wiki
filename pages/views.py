69d6baf58e894b8560a72f6591c5b206bce354c2
Thuraya AlAteeqi <ThurayaAlAteeqi@Thurayas-MacBook-Air.local>
2019-02-06 15:44:15 +0300

    submission

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from pages.models import Page
 
 # Create your views here.
+def page_list(request):
+	context = {
+		"pages" : Page.objects.all(),
+	}
+	return render(request, "list.html", context)
+
+def page_detail(request, page_id):
+
+	context = {
+		"pages" : Page.objects.get(id=page_id),
+	}
+	return render(request, "detail.html", context)
+
